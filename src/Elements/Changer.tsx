6652ec4a0948e31e0b4105700f8123c80900980d
Danylo Sus <danil.sushko19@gmail.com>
2023-08-21 17:59:30 +0300

    Bug fixes

--- src/Elements/Changer.tsx
+++ src/Elements/Changer.tsx
@@ -4,15 +4,15 @@ const Changer = () => {
   const { nextId, pastId } = useIdStore((s) => s);
 
   return (
-    <div className="h-14 w-[112px] bg-white rounded-full relative bottom-7 shadow-lg left-[52px] flex items-center justify-around lg:h-10 lg:w-[80px] lg:left-1/2 lg:transform lg:-translate-x-1/2">
+    <div className="h-14 w-[112px] bg-white rounded-full relative bottom-7 shadow-lg left-[52px] flex items-center justify-around lg:h-10 lg:w-[80px] lg:left-1/2 lg:transform lg:-translate-x-1/2 overflow-hidden">
       <div
-        className="w-1/2 flex justify-around items-center hover:bg-grayish-blue transition-colors h-full"
+        className="w-1/2 flex justify-around items-center hover:bg-grayish-blue transition-colors h-full cursor-pointer"
         onClick={pastId}
       >
         <img src="./icon-prev.svg" alt="icon prev" />
       </div>
       <div
-        className="w-1/2 flex justify-around items-center hover:bg-grayish-blue transition-colors h-full"
+        className="w-1/2 flex justify-around items-center hover:bg-grayish-blue transition-colors h-full cursor-pointer"
         onClick={nextId}
       >
         <img src="./icon-next.svg" alt="icon next" />
